9b6bf2f13f733a845bc6b5823f83aaebf5d916a7
fathxn <fathan0x1@gmail.com>
2024-06-05 22:13:14 +0700

    feat(user): add user service

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"go-crowdfunding/user"
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func main() {
+	// dsn := "root:@tcp(127.0.0.1:3306)/go_crowdfunding?charset=utf8mb4&parseTime=True&loc=Local"
+	// db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	// if err != nil {
+	// 	log.Fatal(err.Error())
+	// }
+
+	// fmt.Println("database connected...")
+
+	// var users []user.User
+	// db.Find(&users)
+	// for _, user := range users {
+	// 	fmt.Println(user.Name)
+	// 	fmt.Println(user.Email)
+	// }
+
+	router := gin.Default()
+	router.GET("/handler", handler)
+	router.Run(":1232")
+}
+
+func handler(c *gin.Context) {
+	dsn := "root:@tcp(127.0.0.1:3306)/go_crowdfunding?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	var users []user.User
+	db.Find(&users)
+
+	c.JSON(http.StatusOK, users)
+}
