ff2e21971b975a8ba93f3e139ae141d17f18e43e
fathxn <fathan0x1@gmail.com>
2024-06-06 19:44:05 +0700

    feat(user): add user repository

--- main.go
+++ main.go
@@ -3,43 +3,21 @@ package main
 import (
 	"go-crowdfunding/user"
 	"log"
-	"net/http"
 
-	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func main() {
-	// dsn := "root:@tcp(127.0.0.1:3306)/go_crowdfunding?charset=utf8mb4&parseTime=True&loc=Local"
-	// db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	// if err != nil {
-	// 	log.Fatal(err.Error())
-	// }
-
-	// fmt.Println("database connected...")
-
-	// var users []user.User
-	// db.Find(&users)
-	// for _, user := range users {
-	// 	fmt.Println(user.Name)
-	// 	fmt.Println(user.Email)
-	// }
-
-	router := gin.Default()
-	router.GET("/handler", handler)
-	router.Run(":1232")
-}
-
-func handler(c *gin.Context) {
 	dsn := "root:@tcp(127.0.0.1:3306)/go_crowdfunding?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 
-	var users []user.User
-	db.Find(&users)
-
-	c.JSON(http.StatusOK, users)
+	userRepoitory := user.NewRepository(db)
+	user := user.User{
+		Name: "Test simpan",
+	}
+	userRepoitory.Save(user)
 }
