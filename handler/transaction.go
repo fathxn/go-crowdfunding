1177c7d4f8987b12c9ddd23a8e5f89e0cd275923
fathxn <fathan0x1@gmail.com>
2024-06-16 14:21:39 +0700

    feat(transaction): add GetCampaignTransactions() handler

--- handler/transaction.go
+++ handler/transaction.go
@@ -1,1 +1,37 @@
 package handler
+
+import (
+	"go-crowdfunding/helper"
+	"go-crowdfunding/transaction"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type transactionHandler struct {
+	service transaction.Service
+}
+
+func NewTransactionHandler(service transaction.Service) *transactionHandler {
+	return &transactionHandler{service: service}
+}
+
+func (h *transactionHandler) GetCampaignTransactions(c *gin.Context) {
+	var input transaction.GetCampaignTransactionsInput
+	err := c.ShouldBindUri(&input)
+	if err != nil {
+		response := helper.APIResponse("failed to get campaign's transactions", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	transactions, err := h.service.GetTransactionsByCampaignID(input)
+	if err != nil {
+		response := helper.APIResponse("failed to get campaign's transactions", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	response := helper.APIResponse("campaign detail", http.StatusOK, "success", transactions)
+	c.JSON(http.StatusOK, response)
+}
