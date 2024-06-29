8c3f07c0c853d33c1356571e1e5ba0caa583d15e
fathxn <fathan0x1@gmail.com>
2024-06-29 16:20:24 +0700

    fix(transaction): fixed transaction json response

--- handler/transaction.go
+++ handler/transaction.go
@@ -74,6 +74,6 @@ func (h *transactionHandler) CreateTransaction(c *gin.Context) {
 		return
 	}
 
-	response := helper.APIResponse("success to create transaction", http.StatusOK, "success", newTransaction)
+	response := helper.APIResponse("success to create transaction", http.StatusOK, "success", transaction.FormatTransaction(newTransaction))
 	c.JSON(http.StatusOK, response)
 }
